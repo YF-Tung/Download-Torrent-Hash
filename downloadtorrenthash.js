a36d5d1ecd67398fa4b8df2a481e609820a59d16
Yu-Fan Tung <yufantung@gmail.com>
2018-08-12 21:40:47 +0800

    Parse html content instead of using regex

--- downloadtorrenthash.js
+++ downloadtorrenthash.js
@@ -2,12 +2,27 @@ function download_torrent_hash() {
     var body = document.getElementsByTagName("BODY")[0]; 
     var bodyText = body.innerHTML;
 
-    body.innerHTML = bodyText.replace(
-        /[a-fA-F0-9]{40}/g,
-        function(match, offset, string) {
+    var parser = new DOMParser();
+    var serializer = new XMLSerializer();
+    var xmlObject = parser.parseFromString('<body>' + body.innerHTML + '</body>', 'text/html');
+
+    if (xmlObject.documentElement.nodeName == 'parsererror') {
+        console.log('Error: cannot parse html body');
+        return;
+    }
+
+    var bodyText = serializer.serializeToString(xmlObject);
+    bodyText = bodyText.replace(
+            /[a-fA-F0-9]{40}/g,
+            function(match, offset, string) {
             return '<a href="magnet:?xt=urn:btih:' + match + '">'
-                + match + '</a>';
-        });
+            + match + '</a>';
+    });
+
+    body.innerHTML = bodyText;
+
+    // Check that script has run correctly
+    document.body.style.border = "25px solid red";
 }
 download_torrent_hash();
 
